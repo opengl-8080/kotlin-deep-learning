000c3e3426ecc993a126259a4aa9bb530bf975ec
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-06-18 20:16:06 +0900

    Add Matrix class

---
+++ core/src/main/kotlin/kondol/Matrix.kt
@@ -0,0 +1,138 @@
+package kondol
+
+import java.util.*
+
+
+class Matrix(private vararg val rows: DoubleArray) {
+    
+    constructor(vararg elements: Double): this(elements)
+    constructor(vararg intRows: IntArray): this(*toDoubleArray(intRows))
+    constructor(vararg elements: Int): this(elements)
+
+    val rowSize: Int
+    val colSize: Int
+    
+    init {
+        if (this.rows.isEmpty()) {
+            throw IllegalArgumentException("rows must have any row.")
+        }
+        if (this.rows[0].isEmpty()) {
+            throw IllegalArgumentException("row must have any elements.")
+        }
+        
+        val firstRowSize = this.rows[0].size
+        this.rows.forEachIndexed { i, row ->
+            if (row.size != firstRowSize) {
+                throw IllegalArgumentException("Expected length is $firstRowSize. However row[$i].length is ${row.size}.")
+            }
+        }
+        
+        this.rowSize = this.rows.size
+        this.colSize = this.rows[0].size
+    }
+
+    operator fun unaryMinus() = this.map { -it }
+
+    operator fun get(rowIndex: Int) = Matrix(this.rows[rowIndex])
+    operator fun get(rowIndex: Int, colIndex: Int) = this.rows[rowIndex][colIndex]
+    
+    operator fun plus(l: Long) = this.map { it + l }
+    operator fun plus(l: Double) = this.map { it + l }
+    operator fun minus(l: Long) = this.map { it - l }
+    operator fun minus(l: Double) = this.map { it - l }
+    operator fun times(l: Long) = this.map { it * l }
+    operator fun times(l: Double) = this.map { it * l }
+    operator fun div(l: Long) = this.map { it / l }
+    operator fun div(l: Double) = this.map { it / l }
+    
+    operator fun plus(other: Matrix) = this.mapWith(other, {thisValue, otherValue -> thisValue + otherValue})
+    operator fun minus(other: Matrix) = this.mapWith(other, {thisValue, otherValue -> thisValue - otherValue})
+    operator fun times(other: Matrix): Matrix {
+        if (this.colSize != other.rowSize) {
+            throw IllegalArgumentException("Expected row size is ${this.colSize}. However actual is ${other.rowSize}.")
+        }
+        
+        val rows = Array(this.rowSize, {DoubleArray(other.colSize)})
+        
+        for (rowIndex in 0 until this.rowSize) {
+            for (colIndex in 0 until other.colSize) {
+                rows[rowIndex][colIndex] = (0 until this.colSize).sumByDouble { 
+                    this[rowIndex, it] * other[it, colIndex]
+                }
+            }
+        }
+        
+        return Matrix(*rows)
+    }
+    
+    fun map(mapper: (Double) -> Double)
+            = this.mapIndexed { _, _, value ->
+                mapper(value)
+            }
+
+    fun mapWith(other: Matrix, mapper: (Double, Double) -> Double)
+            = this.mapIndexed { rowIndex, colIndex, thisValue -> 
+                mapper(thisValue, other[rowIndex, colIndex])
+            }
+    
+    private fun mapIndexed(mapper: (Int, Int, Double) -> Double): Matrix {
+        val rows = Array(this.rowSize, { DoubleArray(this.colSize) })
+
+        this.rows.forEachIndexed { rowIndex, row ->
+            row.forEachIndexed { colIndex, value ->
+                rows[rowIndex][colIndex] = mapper(rowIndex, colIndex, value)
+            }
+        }
+
+        return Matrix(*rows)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is Matrix) return false
+        if (this.rowSize != other.rowSize) return false
+        
+        var i = 0
+        return this.rows.all { row ->
+            Arrays.equals(row, other.rows[i++])
+        }
+    }
+
+    override fun hashCode() = Arrays.deepHashCode(this.rows)
+
+    override fun toString()
+        = this.rows.joinToString(separator = "\n", transform = { row ->
+            row.joinToString(prefix = "[", separator = ", ", postfix = "]")
+        })
+}
+
+operator fun Double.plus(matrix: Matrix) = matrix.map { this + it }
+operator fun Double.minus(matrix: Matrix) = matrix.map { this - it }
+operator fun Double.times(matrix: Matrix) = matrix.map { this * it }
+operator fun Double.div(matrix: Matrix) = matrix.map { this / it }
+
+operator fun Float.plus(matrix: Matrix) = matrix.map { this + it }
+operator fun Float.minus(matrix: Matrix) = matrix.map { this - it }
+operator fun Float.times(matrix: Matrix) = matrix.map { this * it }
+operator fun Float.div(matrix: Matrix) = matrix.map { this / it }
+
+operator fun Long.plus(matrix: Matrix) = matrix.map { this + it }
+operator fun Long.minus(matrix: Matrix) = matrix.map { this - it }
+operator fun Long.times(matrix: Matrix) = matrix.map { this * it }
+operator fun Long.div(matrix: Matrix) = matrix.map { this / it }
+
+operator fun Int.plus(matrix: Matrix) = matrix.map { this + it }
+operator fun Int.minus(matrix: Matrix) = matrix.map { this - it }
+operator fun Int.times(matrix: Matrix) = matrix.map { this * it }
+operator fun Int.div(matrix: Matrix) = matrix.map { this / it }
+
+private fun toDoubleArray(intRows: Array<out IntArray>): Array<DoubleArray> {
+    val array = Array(intRows.size, {DoubleArray(intRows[0].size)})
+    
+    intRows.forEachIndexed { rowIndex, rows -> 
+        rows.forEachIndexed { colIndex, value -> 
+            array[rowIndex][colIndex] = value.toDouble()
+        }
+    }
+    
+    return array
+}
---
+++ core/src/test/kotlin/kondol/MatrixTest.kt
@@ -0,0 +1,697 @@
+package kondol
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+import kotlin.coroutines.experimental.EmptyCoroutineContext.plus
+
+class MatrixTest {
+
+    @Test
+    fun test_toString() {
+        // setup
+        val matrix = Matrix(
+            doubleArrayOf(1.0, 2.0, 3.0),
+            doubleArrayOf(4.0, 5.0, 6.0),
+            doubleArrayOf(7.0, 8.0, 9.0)
+        )
+        
+        // exercise
+        val string = matrix.toString()
+        
+        // verify
+        Assertions.assertThat(string).isEqualTo("""
+        |[1.0, 2.0, 3.0]
+        |[4.0, 5.0, 6.0]
+        |[7.0, 8.0, 9.0]""".trimMargin())
+    }
+
+    @Test
+    fun test_equals_equally_same_shape() {
+        // setup
+        val a = Matrix(
+            doubleArrayOf(1.0, 2.0, 3.0),
+            doubleArrayOf(4.0, 5.0, 6.0)
+        )
+        val b = Matrix(
+            doubleArrayOf(1.0, 2.0, 3.0),
+            doubleArrayOf(4.0, 5.0, 6.0)
+        )
+        
+        // exercise
+        val actual = a == b
+        
+        // verify
+        Assertions.assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun test_equals_not_equally_different_shape_1() {
+        // setup
+        val a = Matrix(
+            doubleArrayOf(1.0, 2.0, 3.0),
+            doubleArrayOf(4.0, 5.0, 6.0)
+        )
+        val b = Matrix(
+            doubleArrayOf(1.0, 2.0),
+            doubleArrayOf(4.0, 5.0)
+        )
+
+        // exercise
+        val actual = a == b
+
+        // verify
+        Assertions.assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun test_equals_not_equally_different_shape_2() {
+        // setup
+        val a = Matrix(
+            doubleArrayOf(1.0, 2.0, 3.0),
+            doubleArrayOf(4.0, 5.0, 6.0)
+        )
+        val b = Matrix(
+            doubleArrayOf(1.0, 2.0, 3.0)
+        )
+
+        // exercise
+        val actual = a == b
+
+        // verify
+        Assertions.assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun test_equals_not_equally_different_shape_3() {
+        // setup
+        val a = Matrix(
+            doubleArrayOf(1.0, 2.0, 3.0, 4.0),
+            doubleArrayOf(4.0, 5.0, 6.0, 7.0)
+        )
+        val b = Matrix(
+            doubleArrayOf(1.0, 2.0, 3.0),
+            doubleArrayOf(4.0, 5.0, 6.0)
+        )
+
+        // exercise
+        val actual = a == b
+
+        // verify
+        Assertions.assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun test_equals_not_equally_null() {
+        // setup
+        val a = Matrix(
+            doubleArrayOf(1.0, 2.0, 3.0, 4.0),
+            doubleArrayOf(4.0, 5.0, 6.0, 7.0)
+        )
+
+        // exercise
+        @Suppress("SENSELESS_COMPARISON")
+        val actual = a == null
+
+        // verify
+        Assertions.assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun test_rowSize() {
+        // setup
+        val matrix = Matrix(
+            doubleArrayOf(1.0, 2.0, 3.0, 4.0),
+            doubleArrayOf(4.0, 5.0, 6.0, 7.0)
+        )
+        
+        // verify
+        Assertions.assertThat(matrix.rowSize).isEqualTo(2)
+    }
+
+    @Test
+    fun test_colSize() {
+        // setup
+        val matrix = Matrix(
+            doubleArrayOf(1.0, 2.0, 3.0, 4.0),
+            doubleArrayOf(4.0, 5.0, 6.0, 7.0)
+        )
+
+        // verify
+        Assertions.assertThat(matrix.colSize).isEqualTo(4)
+    }
+
+    @Test
+    fun test_constructor_validates_that_each_row_has_same_length() {
+        Assertions.assertThatThrownBy { 
+            Matrix(
+                doubleArrayOf(1.0, 2.0),
+                doubleArrayOf(1.0, 2.0, 3.0)
+            )
+        }
+        .isInstanceOf(IllegalArgumentException::class.java)
+        .hasMessage("Expected length is 2. However row[1].length is 3.")
+    }
+
+    @Test
+    fun test_constructor_validates_row_is_not_empty() {
+        Assertions.assertThatThrownBy {
+            Matrix(doubleArrayOf())
+        }
+        .isInstanceOf(IllegalArgumentException::class.java)
+        .hasMessage("row must have any elements.")
+    }
+
+    @Test
+    fun test_A_matrix_has_a_single_row_can_be_constructed_by_raw_double_arguments() {
+        // exercise
+        val matrix = Matrix(1.0, 2.0, 3.0)
+        
+        // verify
+        Assertions.assertThat(matrix).isEqualTo(Matrix(
+            doubleArrayOf(1.0, 2.0, 3.0)
+        ))
+    }
+
+    @Test
+    fun test_A_matrix_can_be_constructed_by_IntArray() {
+        // exercise
+        val matrix = Matrix(
+            intArrayOf(1, 2, 3),
+            intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(matrix).isEqualTo(Matrix(
+            doubleArrayOf(1.0, 2.0, 3.0),
+            doubleArrayOf(4.0, 5.0, 6.0)
+        ))
+    }
+
+    @Test
+    fun test_A_matrix_has_a_single_row_can_be_constructed_by_raw_int_arguments() {
+        // exercise
+        val matrix = Matrix(1, 2, 3)
+
+        // verify
+        Assertions.assertThat(matrix).isEqualTo(Matrix(
+            doubleArrayOf(1.0, 2.0, 3.0)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_plus_Long() {
+        // setup
+        val matrix = Matrix(
+            intArrayOf(1, 2, 3),
+            intArrayOf(4, 5, 6)
+        )
+        
+        // verify
+        Assertions.assertThat(matrix + 3L).isEqualTo(Matrix(
+            intArrayOf(4, 5, 6),
+            intArrayOf(7, 8, 9)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_minus_Long() {
+        // setup
+        val matrix = Matrix(
+            intArrayOf(1, 2, 3),
+            intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(matrix - 3L).isEqualTo(Matrix(
+            intArrayOf(-2, -1, 0),
+            intArrayOf(1, 2, 3)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_times_Long() {
+        // setup
+        val matrix = Matrix(
+            intArrayOf(1, 2, 3),
+            intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(matrix * 3L).isEqualTo(Matrix(
+            intArrayOf(3, 6, 9),
+            intArrayOf(12, 15, 18)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_div_Long() {
+        // setup
+        val matrix = Matrix(
+            intArrayOf(3, 15),
+            intArrayOf(9, 30)
+        )
+
+        // verify
+        Assertions.assertThat(matrix / 3L).isEqualTo(Matrix(
+            intArrayOf(1, 5),
+            intArrayOf(3, 10)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_plus_Double() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(matrix + 3.0).isEqualTo(Matrix(
+                intArrayOf(4, 5, 6),
+                intArrayOf(7, 8, 9)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_minus_Double() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(matrix - 3.0).isEqualTo(Matrix(
+                intArrayOf(-2, -1, 0),
+                intArrayOf(1, 2, 3)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_times_Double() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(matrix * 3.0).isEqualTo(Matrix(
+                intArrayOf(3, 6, 9),
+                intArrayOf(12, 15, 18)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_div_Double() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(3, 15),
+                intArrayOf(9, 30)
+        )
+
+        // verify
+        Assertions.assertThat(matrix / 3.0).isEqualTo(Matrix(
+                intArrayOf(1, 5),
+                intArrayOf(3, 10)
+        ))
+    }
+    
+    @Test
+    fun test_operator_overloading_Double_plus() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(3.0 + matrix).isEqualTo(Matrix(
+                intArrayOf(4, 5, 6),
+                intArrayOf(7, 8, 9)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_Double_minus() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(3.0 - matrix).isEqualTo(Matrix(
+                intArrayOf(2, 1, 0),
+                intArrayOf(-1, -2, -3)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_Double_times() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(3.0 * matrix).isEqualTo(Matrix(
+                intArrayOf(3, 6, 9),
+                intArrayOf(12, 15, 18)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_Double_div() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(3, 4),
+                intArrayOf(6, 2)
+        )
+
+        // verify
+        Assertions.assertThat(12.0 / matrix).isEqualTo(Matrix(
+                intArrayOf(4, 3),
+                intArrayOf(2, 6)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_Long_plus() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(3L + matrix).isEqualTo(Matrix(
+                intArrayOf(4, 5, 6),
+                intArrayOf(7, 8, 9)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_Long_minus() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(3L - matrix).isEqualTo(Matrix(
+                intArrayOf(2, 1, 0),
+                intArrayOf(-1, -2, -3)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_Long_times() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(3L * matrix).isEqualTo(Matrix(
+                intArrayOf(3, 6, 9),
+                intArrayOf(12, 15, 18)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_Long_div() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(3, 4),
+                intArrayOf(6, 2)
+        )
+
+        // verify
+        Assertions.assertThat(12L / matrix).isEqualTo(Matrix(
+                intArrayOf(4, 3),
+                intArrayOf(2, 6)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_Int_plus() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(3 + matrix).isEqualTo(Matrix(
+                intArrayOf(4, 5, 6),
+                intArrayOf(7, 8, 9)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_Int_minus() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(3 - matrix).isEqualTo(Matrix(
+                intArrayOf(2, 1, 0),
+                intArrayOf(-1, -2, -3)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_Int_times() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(3 * matrix).isEqualTo(Matrix(
+                intArrayOf(3, 6, 9),
+                intArrayOf(12, 15, 18)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_Int_div() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(3, 4),
+                intArrayOf(6, 2)
+        )
+
+        // verify
+        Assertions.assertThat(12 / matrix).isEqualTo(Matrix(
+                intArrayOf(4, 3),
+                intArrayOf(2, 6)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_Float_plus() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(3.0f + matrix).isEqualTo(Matrix(
+                intArrayOf(4, 5, 6),
+                intArrayOf(7, 8, 9)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_Float_minus() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(3.0f - matrix).isEqualTo(Matrix(
+                intArrayOf(2, 1, 0),
+                intArrayOf(-1, -2, -3)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_Float_times() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6)
+        )
+
+        // verify
+        Assertions.assertThat(3.0f * matrix).isEqualTo(Matrix(
+                intArrayOf(3, 6, 9),
+                intArrayOf(12, 15, 18)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_Float_div() {
+        // setup
+        val matrix = Matrix(
+                intArrayOf(3, 4),
+                intArrayOf(6, 2)
+        )
+
+        // verify
+        Assertions.assertThat(12.0f / matrix).isEqualTo(Matrix(
+                intArrayOf(4, 3),
+                intArrayOf(2, 6)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_unaryMinus() {
+        // setup
+        val matrix = Matrix(
+            intArrayOf(3, 4),
+            intArrayOf(6, 2)
+        )
+
+        // verify
+        Assertions.assertThat(-matrix).isEqualTo(Matrix(
+            intArrayOf(-3, -4),
+            intArrayOf(-6, -2)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_single_index_access() {
+        // setup
+        val matrix = Matrix(
+            intArrayOf(1, 2),
+            intArrayOf(3, 4)
+        )
+
+        // verify
+        Assertions.assertThat(matrix[1]).isEqualTo(Matrix(
+            intArrayOf(3, 4)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_multi_index_access() {
+        // setup
+        val matrix = Matrix(
+            intArrayOf(1, 2),
+            intArrayOf(3, 4)
+        )
+
+        // verify
+        Assertions.assertThat(matrix[1, 0]).isEqualTo(3.0)
+    }
+
+    @Test
+    fun test_operator_overloading_plus_matrix() {
+        // setup
+        val a = Matrix(
+            intArrayOf(1, 2),
+            intArrayOf(3, 4)
+        )
+        val b = Matrix(
+            intArrayOf(5, 6),
+            intArrayOf(7, 8)
+        )
+
+        // verify
+        Assertions.assertThat(a + b).isEqualTo(Matrix(
+            intArrayOf(6, 8),
+            intArrayOf(10, 12)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_minus_matrix() {
+        // setup
+        val a = Matrix(
+                intArrayOf(1, 2),
+                intArrayOf(3, 4)
+        )
+        val b = Matrix(
+                intArrayOf(5, 6),
+                intArrayOf(7, 8)
+        )
+
+        // verify
+        Assertions.assertThat(a - b).isEqualTo(Matrix(
+                intArrayOf(-4, -4),
+                intArrayOf(-4, -4)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_times_matrix_1() {
+        // setup
+        val a = Matrix(
+            intArrayOf(1, 2),
+            intArrayOf(3, 4)
+        )
+        val b = Matrix(
+            intArrayOf(5),
+            intArrayOf(7)
+        )
+
+        // verify
+        Assertions.assertThat(a * b).isEqualTo(Matrix(
+            intArrayOf(1*5 + 2*7),
+            intArrayOf(3*5 + 4*7)
+        ))
+    }
+
+    @Test
+    fun test_operator_overloading_times_matrix_2() {
+        // setup
+        val a = Matrix(
+                intArrayOf(1, 2),
+                intArrayOf(3, 4)
+        )
+        val b = Matrix(
+                intArrayOf(5, 6, 7),
+                intArrayOf(7, 8, 9)
+        )
+
+        // verify
+        Assertions.assertThat(a * b).isEqualTo(Matrix(
+                intArrayOf(1*5 + 2*7, 1*6 + 2*8, 1*7 + 2*9),
+                intArrayOf(3*5 + 4*7, 3*6 + 4*8, 3*7 + 4*9)
+        ))
+    }
+
+    @Test
+    fun test_times_method_validates_that_this_col_size_equals_to_other_row_size() {
+        // setup
+        val a = Matrix(
+                intArrayOf(1, 2),
+                intArrayOf(3, 4)
+        )
+        val b = Matrix(
+                intArrayOf(5, 6, 7)
+        )
+        
+        // verify
+        Assertions.assertThatThrownBy {
+            // exercise
+            a * b
+        }
+        .isInstanceOf(IllegalArgumentException::class.java)
+        .hasMessage("Expected row size is 2. However actual is 1.")
+    }
+}
