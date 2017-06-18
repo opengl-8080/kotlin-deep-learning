050e8bc8794504422991c0f7453d775b764ea537
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-06-18 20:53:11 +0900

    add sigmoid constant

--- core/src/main/kotlin/kondol/function/ActivationFunction.kt
+++ core/src/main/kotlin/kondol/function/ActivationFunction.kt
@@ -5,4 +5,8 @@ import kondol.matrix.Matrix
 interface ActivationFunction {
     
     operator fun invoke(matrix: Matrix): Matrix
+    
+    companion object {
+        val SIGMOID = SigmoidFunction()
+    }
 }
